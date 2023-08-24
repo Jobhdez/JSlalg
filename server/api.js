42cfe93094566e5e70110f20e180c6a4f62fadbd
Job <hj93@protonmail.com>
2023-08-23 18:29:45 -0700

    add vector addition

--- server/api.js
+++ server/api.js
@@ -1,4 +1,5 @@
 const Mat = require('../src/matrix')
+const Vec = require('../Vector')
 const express = require('express')
 const bodyParser = require('body-parser');
 
@@ -6,26 +7,28 @@ const app = express()
 app.use(express.json())
 app.use(bodyParser.urlencoded({extended: true}));
 
-app.post('/add-mat', (req, res) => {
+app.post('/add', (req, res) => {
     const exp = req.body.expr1
     const exp2 = req.body.expr2
 
 
-    const modifiedExp = exp.replace(/'/g, '"');
-    const modifiedExp2 = exp2.replace(/'/g, '"');
-    let matrix = JSON.parse(modifiedExp);
-    let matrix2 = JSON.parse(modifiedExp2);
-    console.log(matrix)
-    ///let matrix2 = JSON.parse(exp2);
+    const cleanExp = exp.replace(/'/g, '"');
+    const cleanExp2 = exp2.replace(/'/g, '"');
+    let lalgExp = JSON.parse(cleanExp);
+    let lalgExp2 = JSON.parse(cleanExp2);
 
-    let matObj = new Mat.Matrix(matrix)
-    let matObj2 = new Mat.Matrix(matrix2)
+    if (Number.isInteger(lalgExp[0])) {
+        let vec1 =  new Vec.Vector(lalgExp)
+        let vec2 = new Vec.Vector(lalgExp2)
+        let resultVec= vec1.add(vec2)
+        res.json({expr: resultVec.matrix})
+    } else {
+        let matrix = new Mat.Matrix(lalgExp)
+        let matrix2 = new Mat.Matrix(lalgExp2)
+        let resultMatrix = matrix.add(matrix2)
 
-    let addition = matObj.add(matObj2)
-
-    let mat = addition.matrix;
-
-    res.json({expr: mat})
+        res.json({expr: resultMatrix.matrix})
+    }
 })
 
 
