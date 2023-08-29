e8f43ea17c0726f8a292f62af8c6c23a71290400
Job <hj93@protonmail.com>
2023-08-28 23:18:40 -0700

    add abstraction

--- server/utils.js
+++ server/utils.js
@@ -38,5 +38,59 @@ function makeAlgObj(req, method) {
     }
 }
 
+function makeArithObj(req, res, method, method2, op) {
+    const exp = req.body.expr1
+    const exp2 = req.body.expr2
+
+
+    const cleanExp = exp.replace(/'/g, '"');
+    const cleanExp2 = exp2.replace(/'/g, '"');
+    let lalgExp = JSON.parse(cleanExp);
+    let lalgExp2 = JSON.parse(cleanExp2);
+
+    return [lalgExp, lalgExp2]
+}
+
+function computeExp(e1, e2, obj, obj2, op) {
+     if (op === '+') {
+	if (method instanceof Mat.Matrix) {
+	    let compute = new Compute(method.matrix, method2.matrix, lalgExp, lalgExp2).add()
+	    res.json({expr: compute.matrix})
+	}
+	else {
+	    let result = new Compute(method.vector, method2.vector, lalgExp, lalgExp2).add()
+	    res.json({expr: result.vector})
+	}
+    }
+    else {
+	if (method instanceof Mat.Matrix) {
+	    let result = new Compute(method.matrix, method2.matrix2, lalgExp, lalgExp2).sub()
+	    res.json({expr: result.matrix})
+	}
+	else {
+	    let result = new Compute(method.vector, method2.vector, lalgExp, lalgExp2).sub()
+	    res.json({expr: result.vector})
+	}
+    }
+}
+
+class Compute {
+    constructor(lalgObj, lalgObj2, exp, exp2) {
+	this.lalgObj = lalgObj
+	this.lalgObj2 = lalgObj2
+	this.exp = exp
+	this.exp2 = exp
+    }
+    add() {
+	this.lalgObj = this.exp
+	this.lalgObj2 = this.exp2
+	return this.lalgObj.add(lalgObj2)
+    }
+    sub() {
+	this.lalgObj = this.exp
+	this.lalgObj = this.exp2
+	return this.lalgObj.sub(lalgObj2)
+    }
+}
 
 module.exports = {computeAlgExp, makeAlgObj}
