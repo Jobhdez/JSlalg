0eba4d52a652666fcf6f99ad8e7d9c2750497620
Job Hernandez <hj93@protonmail.com>
2024-04-25 19:14:18 -0700

    refactor in ES modules

--- src/lib.js
+++ src/lib.js
@@ -1,93 +1,70 @@
-let elementWise = (op) => {
+// some basic linear algebra :-)
+
+const elementWise = (op) => {
   return (v, v2) => v.map((x, i) => op(x, v2[i]));
 };
 
-let elementWiseOneParam = (op) => {
+const elementWiseOneParam = (op) => {
   return (v) => v.map((x) => op(x));
 };
 
-let add = (e, e2) => {
+const add = (e, e2) => {
   return e + e2;
 };
-let sub = (e, e2) => {
+const sub = (e, e2) => {
   return e - e2;
 };
-let mul = (e, e2) => {
+const mul = (e, e2) => {
   return e * e2;
 };
 
-let addv = elementWise(add);
-let addm = elementWise(addv);
+export const addv = elementWise(add);
+export const addm = elementWise(addv);
 
-let subv = elementWise(sub);
-let subm = elementWise(subv);
+export const subv = elementWise(sub);
+export const subm = elementWise(subv);
 
-let mulv = elementWise(mul);
-function dotp(v, v2) {
+export const mulv = elementWise(mul);
+export function dotp(v, v2) {
   return mulv(v, v2).reduce((accumulator, val) => accumulator + val);
 }
 
-function vecs(v, s) {
+export function vecs(v, s) {
   return v.map((val) => val * s);
 }
 
-function mats(m, s) {
+export function mats(m, s) {
   return m.map((val, i) => vecs(val, s));
 }
 
-let powv = elementWise(Math.pow);
-let powm = elementWise(powv);
+export const powv = elementWise(Math.pow);
+export const powm = elementWise(powv);
 
-let maxv = elementWise(Math.max);
-let maxm = elementWise(maxv);
+export const maxv = elementWise(Math.max);
+export const maxm = elementWise(maxv);
 
-let minv = elementWise(Math.min);
-let minm = elementWise(minv);
+export const minv = elementWise(Math.min);
+export const minm = elementWise(minv);
 
-let expv = elementWiseOneParam(Math.exp);
-let expm = elementWiseOneParam(expv);
+export const expv = elementWiseOneParam(Math.exp);
+export const expm = elementWiseOneParam(expv);
 
-let logv = elementWiseOneParam(Math.log);
-let logm = elementWiseOneParam(logv);
+export const logv = elementWiseOneParam(Math.log);
+export const logm = elementWiseOneParam(logv);
 
-function transpose(matrix) {
+export function transpose(matrix) {
   return matrix[0].map((col, i) => matrix.map((row) => row[i]));
 }
 
-function matMul(m, m2) {
+export function matMul(m, m2) {
   return m.map((row) => transpose(m2).map((col) => dotp(row, col)));
 }
 
 // polynomials
 
-let addp = elementWise(add);
-let subp = elementWise(sub);
+export const addp = elementWise(add);
+export const subp = elementWise(sub);
 
-function degree(poly) {
+export function degree(poly) {
   return poly.length - 1;
 }
-module.exports = {
-  addv,
-  addm,
-  subv,
-  subm,
-  dotp,
-  vecs,
-  mats,
-  powv,
-  powm,
-  maxv,
-  maxm,
-  minv,
-  minm,
-  expv,
-  expm,
-  logv,
-  logm,
-  transpose,
-  matMul,
-  //poly
-  addp,
-  subp,
-  degree,
-};
