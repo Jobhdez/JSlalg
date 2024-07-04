c2ae3cea3e21880bed361485e8c02c14aca0bfd7
Job Hernandez <hj93@protonmail.com>
2024-07-03 21:05:04 -0700

    update app

--- web/src/App.js
+++ web/src/App.js
@@ -3,9 +3,6 @@ import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Home from "./components/Home";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
-import CServe from "./components/CServe";
-import About from "./components/About";
-import OpenSourceProducts from "./components/OpenSourceProducts";
 
 function App() {
   return (
@@ -13,9 +10,6 @@ function App() {
       <Header />
       <Routes>
         <Route exact path="/" element={<Home />} />
-        <Route path="/cserve" element={<CServe />} />
-        <Route path="/open source products" element={<OpenSourceProducts />} />
-        <Route path="/about" element={<About />} />
       </Routes>
       <Footer />
     </Router>
